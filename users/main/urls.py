7fa3280d7a7683881aacbb44798f235ce6bdc55e
ezz <gerkibz@outlook.com>
2021-12-04 11:44:59 +0300

    read json file and display on frontend

---
+++ users/main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet)
+
+app_name = 'main'  # here for namespacing of urls.
+
+urlpatterns = [
+   path('home/',include(router.urls)),
+    path('', views.TheModelView),
+]
---
+++ users/main/views.py
@@ -0,0 +1,35 @@
+from django.http import response
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+import requests
+from rest_framework import viewsets
+from django.shortcuts import render
+from django.http import JsonResponse
+# from themodels.models import Todo
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+@csrf_exempt ## To exempt from default requirement for CSRF tokens to use postman
+def TheModelView(request):
+
+    if (request.method == "GET"):
+        with open('/home/ezz/Documents/gitprojects/crediation/django_user/users/main/fixtures/people.json') as f:
+            data = json.load(f) 
+        # Turn the JSON data into a dict and send as JSON response
+        # return JsonResponse(data, safe=False)
+        return render(request = request,
+                  template_name='main/home.html', 
+                  context = {
+   'data': data
+    })
